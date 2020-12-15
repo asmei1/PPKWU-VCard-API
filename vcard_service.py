fa2bc6c6f06a9a9d55d11f7e830ff5436bea935c
Asmei <kamsykula@gmail.com>
2020-12-15 15:08:42 +0100

    Fixed getting links for each worker

--- vcard_service.py
+++ vcard_service.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 BASE_URL = "https://panoramafirm.pl/szukaj?k="
 
+
 @app.route('/get_list_of_workers/<name>', methods=["GET"])
 def string_api(name):
     if not name:
@@ -13,7 +14,7 @@ def string_api(name):
 
     page = requests.get(BASE_URL + name)
     soup = BeautifulSoup(page.content, "html.parser")
-    links_to_companies = soup.find_all("a")
+    links_to_companies = [a['href'] for a in soup.find_all("a", class_="company-name")]
 
     return {"Hello": "World"}
 
