3fb38f5aa52e4d16a2bf40db475cad671f0afa33
Asmei <kamsykula@gmail.com>
2021-01-12 14:23:59 +0100

    Remove unnecessary endpoint due to further changes in generating result for my api

--- vcard_service.py
+++ vcard_service.py
@@ -65,23 +65,7 @@ def get_worker_vcard():
     vCard = generate_worker_vcard(page)
     return {"vcard", jsonify(vCard)}
 
-@app.route('/get_list_workers_vcards/<name>', methods=["GET"])
-def get_list_with_workers_vcards(name):
-    if not name:
-        return {}, 400
 
-    page = requests.get(BASE_URL + name)
-    soup = BeautifulSoup(page.content, "html.parser")
-    links_to_companies = [a['href'] for a in soup.find_all("a", class_="company-name")]
-
-    workers_vcards = []
-
-    for link in links_to_companies:
-        workers_vcards.append(generate_worker_vcard(link))
-
-    print(workers_vcards)
-
-    return {"Hello": "World"}
 
 
 if __name__ == '__main__':
